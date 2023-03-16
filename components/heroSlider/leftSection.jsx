7aeca0212f92762ed5e8508c4461b39e39982116
munna <dev2.mitaliint@gmail.com>
2023-03-16 14:42:41 +0600

    herosection left side

--- components/heroSlider/leftSection.jsx
+++ components/heroSlider/leftSection.jsx
@@ -8,44 +8,56 @@ const LeftSection = () => {
   return (
     <div>
       <div className="scrollable-content mr-3 border-t-2 border-t-honey bg-primary text-secondary w-full leftHight shadow-hnx">
-        <ul className={`${styles.dropdown_content} text-lg`}>
+        <ul className={`${styles.dropdown_content} text-xl tracking-wider`}>
           <li className="space-x-2 py-5 px-9 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">Royal Honey</a>
+            <a className=" font-semibold">Male Enhancement</a>
+            <span>
+              <TiChevronRight className="text-secondary" />
+            </span>
+          </li>
+          <li className="space-x-2 py-5 px-9 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
+            <a className=" font-semibold">Female Enhancement</a>
+            <span>
+              <TiChevronRight className="text-secondary" />
+            </span>
+          </li>
+          <li className="space-x-2 py-5 px-9 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
+            <a className=" font-semibold">Honey</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
           <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">Black Bull Honey</a>
+            <a className=" font-semibold">Liquid Shots</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
           <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">Blue Bull Honey</a>
+            <a className=" font-semibold">Pills</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
           <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">Extumas Honey</a>
+            <a className=" font-semibold">Condoms</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
           <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">VIP Honey</a>
+            <a className=" font-semibold">Rolling Paper</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
           <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
-            <a className=" font-semibold">Gold Honey</a>
+            <a className=" font-semibold">Cone Paper</a>
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
           </li>
-          <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
+          {/* <li className="py-5 px-9 space-x-2 flex justify-between items-center cursor-pointer hover:bg-white shadow-sm hover:shadow-hnx">
             <a className=" font-semibold">Leopard Miracle</a>
             <span>
               <TiChevronRight className="text-secondary" />
@@ -62,7 +74,7 @@ const LeftSection = () => {
             <span>
               <TiChevronRight className="text-secondary" />
             </span>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
