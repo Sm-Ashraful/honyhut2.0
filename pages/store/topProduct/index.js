b9225a1d5ee43d6825ffd6b9e627ad1166a4bdd5
Sm Ashraful <mitali.developer1@gmail.com>
2023-03-16 10:04:05 +0600

    solve store page issue

--- pages/store/topProduct/index.js
+++ pages/store/topProduct/index.js
@@ -11,19 +11,7 @@ const Products = () => {
       <h2 className="mb-0 ">Top Products</h2>
       <div className="grid grid-cols-2 m-5 gap-1 md:grid-cols-5 sm:grid-cols-3">
         {product.map((item, index) => {
-          return (
-            <Card
-              key={index}
-              name={item.name}
-              image={item.image}
-              category={item.category}
-              quote={item.quote}
-              quantity={item.quantity}
-              price={item.price}
-              categoryType={`products`}
-              percentage={`20%`}
-            />
-          );
+          return <Card key={index} product={item} percentage={`20%`} />;
         })}
       </div>
     </section>
