126503fe4f677f48cce01759c8927cd10e7b4e10
munna <dev2.mitaliint@gmail.com>
2023-03-17 16:50:21 +0600

    products page

--- components/all-products/index.jsx
+++ components/all-products/index.jsx
@@ -1,84 +1,59 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
-import Male from "../../Assets/Category/Male.jpeg"
-
+import Male from "../../Assets/Category/Male.jpeg";
+import Female from "../../Assets/Category/Fmale.jpeg";
+import Condoms from "../../Assets/Category/Condoms.jpeg";
+import Cannabies from "../../Assets/Category/Cannabies.jpeg";
 
 const Products = () => {
   return (
     <div>
-    <div className="mr-3 border-t-2 bg-primary w-full flex">
-      <div className="flex ">
-        <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
-        <div className='relative flex shadow-sm'>
-        <div className=' p-2 h-16 w-16 relative'>
-        <Image fill cover src={Male} />
-        </div>
-          <h1 className=" font-bold pt-7">Male Enhancement</h1>
-        </div>
-          <div className="flex pt-8 md:pt-3 space-x-5 justify-between">
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey active">
-                Honey
-              </button>
-            </Link>
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey">
-                Pills
-              </button>
-            </Link>
-            <Link href="#">
-              <button className=" text-lg text-gray hover:text-honey hover:underline">
-                Liquid Shots
-              </button>
-            </Link>
+      <div className="mr-3 bg-primary w-full h-auto relative top-10">
+        <div className="grid grid-cols-2 md:grid-cols-4">
+
+          <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
+            <div className="relative flex flex-col justify-center items-center hover:text-honey">
+              <div className=" pt-2 h-24 w-32 relative items-center">
+                <Image fill cover src={Male} />
+              </div>
+              <h1 className=" font-bold pt-7 ml-1 text-center">Male Enhancement</h1>
+            </div>
           </div>
-        </div>
-        <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
-          <h1 className="font-bold">Female Enhancement</h1>
-          <div className="flex pt-8 md:pt-3 space-x-5 justify-between">
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey">
-                Pills
-              </button>
-            </Link>
-            <Link href="#">
-              <button className=" text-lg text-gray hover:text-honey hover:underline">
-                Liquid Shots
-              </button>
-            </Link>
+
+          <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
+            <div className="relative flex flex-col justify-center items-center hover:text-honey">
+              <div className=" pt-2 h-24 w-32 relative items-center">
+                <Image fill cover src={Female} />
+              </div>
+              <h1 className=" font-bold pt-7 ml-1 text-center">Female Enhancement</h1>
+            </div>
           </div>
-        </div>
-        <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
-          <h1 className="font-bold ">Condoms</h1>
-          <div className="flex pt-8 md:pt-3 space-x-5 justify-between">
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey">
-                Male Condoms
-              </button>
-            </Link>
+          
+          <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
+            <div className="relative flex flex-col justify-center items-center hover:text-honey">
+              <div className=" pt-2 h-24 w-32 relative items-center">
+                <Image fill cover src={Condoms} />
+              </div>
+              <h1 className=" font-bold pt-7 ml-1 text-center">Condoms</h1>
+            </div>
           </div>
-        </div>
-        <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
-          <h1 className="font-bold">Cannabies Accessories</h1>
-          <div className="flex pt-8 md:pt-3 space-x-5 justify-between">
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey">
-                Rolling Paper
-              </button>
-            </Link>
-            <Link href="#">
-              <button className="text-lg  text-gray hover:text-honey">
-                Cone Paper
-              </button>
-            </Link>
+
+          <div className="p-5 justify-between items-center cursor-pointer hover:bg-white shadow-md">
+            <div className="relative flex flex-col justify-center items-center hover:text-honey">
+              <div className=" pt-2 h-24 w-32 relative items-center">
+                <Image fill cover src={Cannabies} />
+              </div>
+              <h1 className=" font-bold pt-7 ml-1 text-center">Cannabies Accessories</h1>
+            </div>
           </div>
+          
+
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Products;
