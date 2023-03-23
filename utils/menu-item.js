2c8c3c32a979dda98f4ec3726dee0184bd1a1dc1
munna <dev2.mitaliint@gmail.com>
2023-03-23 17:05:48 +0600

    product api

--- utils/menu-item.js
+++ utils/menu-item.js
@@ -7,41 +7,190 @@ export const menuItem = [
         submenu: [
           {
             title: "Royal Honey VIP",
+            details: [
+              {
+                name: "Royal Honey VIP 20g Sachet",
+                image: "../",
+                unit: "12 Sachet Per Box, 48 Box Per Carton",
+                price: 27,
+                MOQ: "96 Box",
+              },
+              {
+                name: "Royal Honey VIP 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box, 28 Box Per Carton",
+                price: 38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Etumax",
+            details: [
+              {
+                name: "Etumax Honey 20g Sachet",
+                image: "../",
+                unit: "12 Sachet Per Box, 60 Box Per Carton",
+                price: 27,
+                MOQ: "120 Box",
+              },
+              {
+                name: "Etumax Honey 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box ,28 Box Per Carton",
+                price: 38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Black Bull",
+            details: [
+              {
+                name: "Black Bull Honey 22g Pouches",
+                image: "../",
+                unit: "12 Poches Per Box, 48 Box Per Carton",
+                price: $35,
+                MOQ: "96 Box",
+              },
+              {
+                name: "Black Bull Honey 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box, 28 Box Per Carton",
+                price: $38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Red Bull",
+            details: [
+              {
+                name: "Red Bull Honey 22g Pouches",
+                image: "../",
+                unit: "12 Poches Per Box, 40 Box Per Carton",
+                price: $35,
+                MOQ: "80 Box",
+              },
+              {
+                name: "Red Bull Honey 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box, 28 Box Per Carton",
+                price: $38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Blue Bull",
+            details: [
+              {
+                name: "Blue Bull Honey 22g Pouches",
+                image: "../",
+                unit: "12 Poches Per Box, 40 Box Per Carton",
+                price: $35,
+                MOQ: "80 Box",
+              },
+              {
+                name: "Blue Bull Honey 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box, 28 Box Per Carton",
+                price: $38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Leopard Miracle",
+            details: [
+              {
+                name: "Leopard Miracle Honey 15g Sachet",
+                image: "../",
+                unit: "12 Sachet Per Box, 40 Box Per Carton",
+                price: $30,
+                MOQ: "80 Box",
+              },
+              {
+                name: "Leopard Miracle Honey 22g Pouches",
+                image: "../",
+                unit: "15 Pouches Per Box, 28 Box Per Carton",
+                price: $38,
+                MOQ: "280 Box",
+              },
+            ],
           },
           {
             title: "Secret Miracle Honey",
+            details: [
+              {
+                name: "Secret Miracle Honey 15g Sachet",
+                image: "../",
+                unit: "12 Poches Per Box, 40 Box Per Carton",
+                price: $32,
+                MOQ: "80 Box",
+              },
+            ]
           },
           {
             title: "Vitamax Double Shot",
+            details: [
+              {
+                name: "Vitamax Double Shot 20g Sachet",
+                image: "../",
+                unit: "10 Poches Per Box, 48 Box Per Carton",
+                price: $37,
+                MOQ: "97 Box",
+              },
+            ]
           },
         ],
       },
+
       {
         title: "Liquid Shots",
         submenu: [
           {
             title: "Rhino",
+            details: [
+              {
+                name: "Rhino 69 Liquid Shot ",
+                image: "../",
+                unit: "12 Bottles Per Box",
+                price: $40,
+                MOQ: "20 Box",
+              },
+              {
+                name: "Rhino Platinum Liquid Shot",
+                image: "../",
+                unit: "12 Bottles Per Box",
+                price: $40,
+                MOQ: "20 Box",
+              },
+            ]
           },
           {
             title: "Hard Steel",
+            details: [
+              {
+                name: "Hard Steel Liquid Shot",
+                image: "../",
+                unit: "12 Bottles Per Box",
+                price: $40,
+                MOQ: "20 Box",
+              },
+            ]
           },
           {
             title: "Superman",
+            details: [
+              {
+                name: "Super Man Liquid Shot",
+                image: "../",
+                unit: "12 Bottles Per Box",
+                price: $40,
+                MOQ: "20 Box",
+              },
+            ]
           },
         ],
       },
@@ -50,18 +199,192 @@ export const menuItem = [
         submenu: [
           {
             title: "Viagra",
+            details: [
+              {
+                name: "Viagra Single Pack",
+                image: "../",
+                unit: "1 Pcs Per Box",
+                price: $250,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Viagra 4 in 1 Pack",
+                image: "../",
+                unit: "4 Pcs Per Box",
+                price: $350,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Viagra 30 Pcs Bottle",
+                image: "../",
+                unit: "30 Pcs Per Bottle",
+                price: $800,
+                MOQ: "100 Bottles",
+              },
+            ]
           },
           {
             title: "Cialis",
+            details: [
+              {
+                name: "Cialis Twin Pack",
+                image: "../",
+                unit: "2 Pcs Per Box",
+                price: $250,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Cialis Bottle",
+                image: "../",
+                unit: "30 Pcs Per Bottle",
+                price: $800,
+                MOQ: "100 Bottles",
+              },
+            ]
           },
           {
             title: "ChingAL ing",
+            details: [
+              {
+                name: "ChingAL ing Single Bag",
+                image: "../",
+                unit: "1 Pcs Per Bag",
+                price: $500,
+                MOQ: "500 Bag",
+              },
+              {
+                name: "ChingAL ing 25 Pcs Pack",
+                image: "../",
+                unit: "25 Pcs Per Pack",
+                price: $900,
+                MOQ: "100 Pack",
+              },
+            ]
           },
           {
             title: "Kangaroo",
+            details: [
+              {
+                name: "Kangaroo Green Single Pack",
+                image: "../",
+                unit: "30 Pack Per Box, 57 Box Per Carton",
+                price: $500,
+                MOQ: "114 Box",
+              },
+              {
+                name: "Kangaroo Blue Single Pack Small Box",
+                image: "../",
+                unit: "30 Pack Per Box, 57 Box Per Carton",
+                price: $500,
+                MOQ: "114 Box",
+              },
+              {
+                name: "Kangaroo Blue Single Pack Large Box",
+                image: "../",
+                unit: "36 Pack Per Box, 48 Box Per Carton",
+                price: $500,
+                MOQ: "96 Box",
+              },
+            ]
           },
           {
             title: "Rhino",
+            details: [
+              {
+                name: "Rhino 69 500K Twin Pack",
+                image: "../",
+                unit: "24 Pack 48 Pills Per Box",
+                price: $1000,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 7 500K Twin Pack",
+                image: "../",
+                unit: "24 Pack 48 Pills Per Box",
+                price: $1000,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 8 500K Twin Pack",
+                image: "../",
+                unit: "24 Pack 48 Pills Per Box",
+                price: $1000,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 7 Platinum 12000 Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1000,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino Rush 70 13000 Trio Pack",
+                image: "../",
+                unit: "24 Pack 72 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Platinum 10K Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box, 12 Large Card+12 Small Card",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 7 3000 Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Blue Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Go Rhino Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 17 12000 Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 250K Single Pack",
+                image: "../",
+                unit: "24 Pack 24 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rush 78 5 in 1 Pack",
+                image: "../",
+                unit: "24 Pack 120 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+              {
+                name: "Rhino 7 5000 Single Pack",
+                image: "../",
+                unit: "24 Pack 120 Pills Per Box",
+                price: $1800,
+                MOQ: "100 Box",
+              },
+            ]
+          },
+          {
+            title: "Magnum",
           },
           {
             title: "Premier ZEN",
