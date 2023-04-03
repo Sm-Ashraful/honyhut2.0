de249cf484b3d05239adac051a93648e07d01a4a
munna <dev2.mitaliint@gmail.com>
2023-04-03 13:27:04 +0600

    utill honey section

--- components/Dropdown/index.js
+++ components/Dropdown/index.js
@@ -26,9 +26,12 @@ const Dropdown = ({ submenus, depthLevel, dropdown }) => {
           <div
             className={`py-5 px-5 flex flex-col h-full shadow-right ${width}`}
           >
+            <div>
             <span className=" text-primary-red border-b-2">
               {dropMenu.title}
             </span>
+            
+            </div>
             <DropdownModal items={dropMenu} />
           </div>
         );
