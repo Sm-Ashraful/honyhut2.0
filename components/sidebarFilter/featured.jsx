b0994c92e52b111d4e7d1c342c7c4a4b9bdb41a6
munna <dev2.mitaliint@gmail.com>
2023-05-08 17:19:08 +0600

    product

--- components/sidebarFilter/featured.jsx
+++ components/sidebarFilter/featured.jsx
@@ -21,7 +21,7 @@ const FeaturedPage = () => {
   return (
     <div class="flex ml-6 items-center">
       <div class="relative">
-        <select class="text-xl font-medium rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:border-indigo-500 px-6">
+        <select class="text-xl font-medium rounded border appearance-none border-gray-300 py-2 pl-4 focus:outline-none focus:ring-2 focus:border-indigo-500">
           <option>Short By</option>
           <option>Best Selling</option>
           <option>Alphabetically, A - Z</option>
