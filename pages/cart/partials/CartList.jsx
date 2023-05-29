7ba876c7383ac223e75c0b410303807e3ceb3ccb
mitali-international-limited <124981715+mitali-international-limited@users.noreply.github.com>
2023-05-29 17:32:29 +0600

    adding map

--- pages/cart/partials/CartList.jsx
+++ pages/cart/partials/CartList.jsx
@@ -30,76 +30,77 @@ const CartList = (props) => {
             </tr>
           </thead>
           <tbody className="text-sm md:text-lg bg-gray-50">
-            {data.map((item) => (
-              <tr
-                key={item.id}
-                className="border border-l-0 border-r-0 border-gray "
-              >
-                <td className="py-5 px-2 mr-3 text-center border-r  text-gray">
-                  <div>
-                    <span
-                      className="mx-4 font-bold cursor-pointer hover:text-primary-red flex justify-center items-center"
-                      onClick={() => handleRemoveFromCart(item)}
-                    >
-                      <MdClose />
-                    </span>
-                  </div>
-                </td>
-                <td className="p-2 h-16 md:h-20 w-20 md:w-28 shadow-sm border-r">
-                  <img
-                    className="w-full h-full bg-center"
-                    src={item.image}
-                    alt={"Image"}
-                  />
-                </td>
-                <td className="py-5 px-2 text-end mr-3 border-r">
-                  {item.name}
-                </td>
-                <td className="py-5 px-2 text-end border-r">
-                  {typeof item.offer === "number" ? (
-                    <>
-                      {item.offer && (
-                        <span>
-                          $
-                          {(
-                            item.price -
-                            (item.price * item.offer) / 100
-                          ).toFixed(2)}
-                        </span>
-                      )}
-                    </>
-                  ) : (
-                    <span>${item.price.toFixed(2)}</span>
-                  )}
-                </td>
-                <td className="py-5 px-2 flex justify-end items-center ml-2 border-r">
-                  <div className="flex flex-col justify-center items-center rounded-full py-2 ">
-                    <span
-                      className="mx-2 py-2 font-bold cursor-pointer hover:text-primary-red bg-gray rounded-lg"
-                      onClick={() => handleDecreaseQuantity(item)}
-                    >
-                      <AiOutlineMinus />
-                    </span>
-                    {item.quantity}
-                    <span
-                      className="mx-2 py-2 font-bold cursor-pointer hover:text-primary-red bg-gray rounded-full"
-                      onClick={() => handleIncreaseQuantity(item)}
-                    >
-                      <AiOutlinePlus />
-                    </span>
-                  </div>
-                </td>
-                <td className="py-5 px-2 text-end">
-                  ${" "}
-                  {(
-                    item.quantity *
-                    (typeof item.offer === "number"
-                      ? item.price - (item.price * item.offer) / 100
-                      : item.price)
-                  ).toFixed(2)}
-                </td>
-              </tr>
-            ))}
+            {data &&
+              data.map((item) => (
+                <tr
+                  key={item.id}
+                  className="border border-l-0 border-r-0 border-gray "
+                >
+                  <td className="py-5 px-2 mr-3 text-center border-r  text-gray">
+                    <div>
+                      <span
+                        className="mx-4 font-bold cursor-pointer hover:text-primary-red flex justify-center items-center"
+                        onClick={() => handleRemoveFromCart(item)}
+                      >
+                        <MdClose />
+                      </span>
+                    </div>
+                  </td>
+                  <td className="p-2 h-16 md:h-20 w-20 md:w-28 shadow-sm border-r">
+                    <img
+                      className="w-full h-full bg-center"
+                      src={item.image}
+                      alt={"Image"}
+                    />
+                  </td>
+                  <td className="py-5 px-2 text-end mr-3 border-r">
+                    {item.name}
+                  </td>
+                  <td className="py-5 px-2 text-end border-r">
+                    {typeof item.offer === "number" ? (
+                      <>
+                        {item.offer && (
+                          <span>
+                            $
+                            {(
+                              item.price -
+                              (item.price * item.offer) / 100
+                            ).toFixed(2)}
+                          </span>
+                        )}
+                      </>
+                    ) : (
+                      <span>${item.price.toFixed(2)}</span>
+                    )}
+                  </td>
+                  <td className="py-5 px-2 flex justify-end items-center ml-2 border-r">
+                    <div className="flex flex-col justify-center items-center rounded-full py-2 ">
+                      <span
+                        className="mx-2 py-2 font-bold cursor-pointer hover:text-primary-red bg-gray rounded-lg"
+                        onClick={() => handleDecreaseQuantity(item)}
+                      >
+                        <AiOutlineMinus />
+                      </span>
+                      {item.quantity}
+                      <span
+                        className="mx-2 py-2 font-bold cursor-pointer hover:text-primary-red bg-gray rounded-full"
+                        onClick={() => handleIncreaseQuantity(item)}
+                      >
+                        <AiOutlinePlus />
+                      </span>
+                    </div>
+                  </td>
+                  <td className="py-5 px-2 text-end">
+                    ${" "}
+                    {(
+                      item.quantity *
+                      (typeof item.offer === "number"
+                        ? item.price - (item.price * item.offer) / 100
+                        : item.price)
+                    ).toFixed(2)}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
 
