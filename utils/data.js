e7724a0fd3f0a216d3dc3faf291817dc389494e6
munna <dev2.mitaliint@gmail.com>
2023-05-31 17:15:56 +0600

    product database

--- utils/data.js
+++ utils/data.js
@@ -1735,4 +1735,128 @@ const data = [
       support: 'Guaranteed Satisfaction!',
     },
   },
+  {
+    id: 57,
+    name: 'Rhino Platinum Liquid Shot',
+    boxStyle: 'Gift',
+    price: 19,
+    image: [],
+    description:
+      'Make the intimate moments with your partner last longer and feel better.',
+    unit: '12 Sachet Per Box',
+    reviews: 5,
+    brand: 'Royal Honey',
+    Subcategory: 'Honey',
+    Category: 'Male Enhancement',
+    createdBy: 'Developer 1',
+    updatedAt: '13/04/2023',
+    details: {
+      title:
+        'Royal Honey enriches your body with a sudden boost of vitality and stamina so you can enjoy your romantic pleasures without the struggles of burnout or an unwelcoming early climax.',
+      ProductBenefits: [
+        'PREMIUM SEXUAL STIMULANT: Royal honey is an all-natural way to stimulate sexual appetite and spark desire between you and your loved one, or any partner you want to attract.',
+        'MAXIMIZE ENDURANCE: Unleash your inner alpha male and discover endurance without exhaustion',
+        'STRONGER ERECTIONS: Never get tired in the bedroom and enjoy unbounded peak performance',
+        'NATURAL INGREDIENTS: Fuel your body from the inside out to naturally boost your testosterone levels',
+        'IMPROVES CONFIDENCE: Skyrocket your sexual performance and become a king in the bedroom',
+      ],
+      Origin: 'MADE IN MALAYSIA',
+      included:
+        '100% authentic honey sachets, formulated from the highest-quality Malaysian ingredients Two Boxes of 12 sachets, 20 grams each',
+      support: 'Guaranteed Satisfaction!',
+    },
+  },
+  {
+    id: 58,
+    name: 'Rhino Platinum Liquid Shot',
+    boxStyle: 'Hexagonal',
+    price: 19,
+    image: [],
+    description:
+      'Make the intimate moments with your partner last longer and feel better.',
+    unit: '12 Sachet Per Box',
+    reviews: 5,
+    brand: 'Royal Honey',
+    Subcategory: 'Honey',
+    Category: 'Male Enhancement',
+    createdBy: 'Developer 1',
+    updatedAt: '13/04/2023',
+    details: {
+      title:
+        'Royal Honey enriches your body with a sudden boost of vitality and stamina so you can enjoy your romantic pleasures without the struggles of burnout or an unwelcoming early climax.',
+      ProductBenefits: [
+        'PREMIUM SEXUAL STIMULANT: Royal honey is an all-natural way to stimulate sexual appetite and spark desire between you and your loved one, or any partner you want to attract.',
+        'MAXIMIZE ENDURANCE: Unleash your inner alpha male and discover endurance without exhaustion',
+        'STRONGER ERECTIONS: Never get tired in the bedroom and enjoy unbounded peak performance',
+        'NATURAL INGREDIENTS: Fuel your body from the inside out to naturally boost your testosterone levels',
+        'IMPROVES CONFIDENCE: Skyrocket your sexual performance and become a king in the bedroom',
+      ],
+      Origin: 'MADE IN MALAYSIA',
+      included:
+        '100% authentic honey sachets, formulated from the highest-quality Malaysian ingredients Two Boxes of 12 sachets, 20 grams each',
+      support: 'Guaranteed Satisfaction!',
+    },
+  },
+  {
+    id: 59,
+    name: 'Rhino Platinum Liquid Shot',
+    boxStyle: 'Spoon',
+    price: 19,
+    image: [],
+    description:
+      'Make the intimate moments with your partner last longer and feel better.',
+    unit: '12 Sachet Per Box',
+    reviews: 5,
+    brand: 'Royal Honey',
+    Subcategory: 'Honey',
+    Category: 'Male Enhancement',
+    createdBy: 'Developer 1',
+    updatedAt: '13/04/2023',
+    details: {
+      title:
+        'Royal Honey enriches your body with a sudden boost of vitality and stamina so you can enjoy your romantic pleasures without the struggles of burnout or an unwelcoming early climax.',
+      ProductBenefits: [
+        'PREMIUM SEXUAL STIMULANT: Royal honey is an all-natural way to stimulate sexual appetite and spark desire between you and your loved one, or any partner you want to attract.',
+        'MAXIMIZE ENDURANCE: Unleash your inner alpha male and discover endurance without exhaustion',
+        'STRONGER ERECTIONS: Never get tired in the bedroom and enjoy unbounded peak performance',
+        'NATURAL INGREDIENTS: Fuel your body from the inside out to naturally boost your testosterone levels',
+        'IMPROVES CONFIDENCE: Skyrocket your sexual performance and become a king in the bedroom',
+      ],
+      Origin: 'MADE IN MALAYSIA',
+      included:
+        '100% authentic honey sachets, formulated from the highest-quality Malaysian ingredients Two Boxes of 12 sachets, 20 grams each',
+      support: 'Guaranteed Satisfaction!',
+    },
+  },
+  {
+    id: 60,
+    name: 'Rhino Platinum Liquid Shot',
+    boxStyle: 'Nozzle',
+    price: 19,
+    image: [],
+    description:
+      'Make the intimate moments with your partner last longer and feel better.',
+    unit: '12 Sachet Per Box',
+    reviews: 5,
+    brand: 'Royal Honey',
+    Subcategory: 'Honey',
+    Category: 'Male Enhancement',
+    createdBy: 'Developer 1',
+    updatedAt: '13/04/2023',
+    details: {
+      title:
+        'Royal Honey enriches your body with a sudden boost of vitality and stamina so you can enjoy your romantic pleasures without the struggles of burnout or an unwelcoming early climax.',
+      ProductBenefits: [
+        'PREMIUM SEXUAL STIMULANT: Royal honey is an all-natural way to stimulate sexual appetite and spark desire between you and your loved one, or any partner you want to attract.',
+        'MAXIMIZE ENDURANCE: Unleash your inner alpha male and discover endurance without exhaustion',
+        'STRONGER ERECTIONS: Never get tired in the bedroom and enjoy unbounded peak performance',
+        'NATURAL INGREDIENTS: Fuel your body from the inside out to naturally boost your testosterone levels',
+        'IMPROVES CONFIDENCE: Skyrocket your sexual performance and become a king in the bedroom',
+      ],
+      Origin: 'MADE IN MALAYSIA',
+      included:
+        '100% authentic honey sachets, formulated from the highest-quality Malaysian ingredients Two Boxes of 12 sachets, 20 grams each',
+      support: 'Guaranteed Satisfaction!',
+    },
+  },
 ];
