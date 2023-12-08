4fbde3b5cd637b6766c8f4454efa5dba4ebe410d
Sm-Ashraful <smashraful.dev@gmail.com>
2023-12-08 17:03:01 +0600

    dispatch

--- Store/product/product.action.js
+++ Store/product/product.action.js
@@ -2,9 +2,9 @@ import { PRODUCT_ACTION_CONST } from "./product.types";
 
 // new action
 export const setProducts = (products) => {
-  return {
-    type: PRODUCT_ACTION_CONST.SET_ALL_PRODUCTS_SUCCESS,
-    payload: products,
-  };
+  // return {
+  //   type: PRODUCT_ACTION_CONST.SET_ALL_PRODUCTS_SUCCESS,
+  //   payload: products,
+  // };
 };
 // modified actrion
